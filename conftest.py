1099f741cde366ee3ce849729ea675730fe88828
Airon (Maxim Lysov) <maksim285@mail.ru>
2023-04-02 12:05:07 +0700

    finish

--- conftest.py
+++ conftest.py
@@ -1,36 +1,38 @@
 import os
 import allure
 import pytest
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import browser
 
 from utils import attach
 
-# DEFAULT_BROWSER_VERSION = "100.0"
+DEFAULT_BROWSER_VERSION = "100.0"
 
 
-# def pytest_addoption(parser):
-#     parser.addoption(
-#         '--browser_version',
-#         default='100.0'
-#     )
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
 
-# @pytest.fixture(scope='session', autouse=True)
-# def load_env():
-#     load_dotenv()
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @allure.step('Open registration form')
 @pytest.fixture(scope="session")
 def open_browser(request):
-    # browser_version = request.config.getoption('--browser_version')
-    # browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+    browser_version = request.config.getoption('--browser_version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
 
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
@@ -39,8 +41,11 @@ def open_browser(request):
 
     options.capabilities.update(selenoid_capabilities)
 
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options)
 
     # browser = Browser(Config(driver))
